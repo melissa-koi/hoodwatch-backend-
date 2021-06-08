c9a8ca7503811ca0d7f2dfe1e21fb4d6a2c0108d
melissa-koi <melissawangui3@gmail.com>
2021-06-09 02:47:48 +0300

    add allow methods to cors

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -157,6 +157,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CORS_ORIGIN_ALLOW_ALL = True # If this is used then `CORS_ORIGIN_WHITELIST` will not have any effect
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 
 # Default primary key field type
